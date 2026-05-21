d1b7908ee174d8f68d3fdcb22bbb40e46218f579
Aleksy <128327771+MerchantGuildMember@users.noreply.github.com>
2026-05-21 19:24:33 +0100

    updated create_monitors to create a postgres timestamp by default

--- migrations/000002_create_monitors.up.sql
+++ migrations/000002_create_monitors.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE monitors (
     url varchar(2048) NOT NULL UNIQUE, -- tough luck if your url is any longer
     website_name varchar(50) NOT NULL,
     website_description varchar(500),
-    created_at TIMESTAMPTZ NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT Now(),
     first_created_by varchar(36),
     FOREIGN KEY (first_created_by) REFERENCES
                       users(user_id)
